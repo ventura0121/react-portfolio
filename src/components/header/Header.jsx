b162cb3dd5f7852216eaff5e26372cb5a4a5ece1
ventura0121 <ventura0121@gmail.com>
2022-09-15 02:47:46 -0400

    Added media links, css styles, and image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Header.css'
-
+import CTA from './CTA'
+import ME from '../../assets/ME.png'
+import Social from './Social'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <div className="container header__container">
+            <h5>Hello I'm</h5>
+            <h1>Felipe Ventura</h1>
+            <h5 className="text-light">Fullstack Developer</h5>
+            <CTA />
+            <Social />
+
+            <div className='me'>
+                <img src ={ME} alt=""/>
+            </div>
+
+            <a href="#contact" className='scroll__down'>Scroll Down</a>
+        </div>
+    </header>
   )
 }
 
---
+++ src/components/header/Social.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+const Social = () => {
+  return (
+    <div className='social_header'>
+        <a href="https://www.linkedin.com/in/felipeventura16/" target ="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/ventura0121" target ="_blank" rel="noreferrer"><FaGithub/></a>
+    </div>
+  )
+}
+
+export default Social
