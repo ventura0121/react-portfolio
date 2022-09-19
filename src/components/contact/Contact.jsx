0be1e7dcaae8ede2f1e35b457352ef930dc3f3c3
ventura0121 <ventura0121@gmail.com>
2022-09-19 18:01:33 -0400

    Completed Contact Me

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="contact__container">
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>ventura0121@gmail.com</h5>
+            <a href="mailto:ventura0121@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsLinkedin className='contact__option-icon'/>
+            <h4>LinkedIn</h4>
+            <h5>Let's Connect!</h5>
+            <a href="https://linkedin.com/in/felipeventura16" target="_blank">Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+          </form>
+      </div>
+      
+    </section>
   )
 }
 
