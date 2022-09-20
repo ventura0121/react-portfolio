d7e1e2972ff7f5a9312c896c817bac2ef7c02916
ventura0121 <ventura0121@gmail.com>
2022-09-20 01:47:13 -0400

    fixed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import './Footer.css'
 import {FaFacebookF} from 'react-icons/fa'
 import {FiInstagram} from 'react-icons/fi'
-import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
   return (
     <footer>
-      <a href = "#" className ="footer__logo">VENTURA</a>
+      <a href = "#" className ="footer__logo">Felipe's Portfolio</a>
 
       <ul className='permalinks'>
-        <li><a href="#"></a>Home</li>
-        <li><a href="#about"></a>About</li>
-        <li><a href="#portfolio"></a>Experience</li>
-        <li><a href="#hobbies"></a>Services</li>
-        <li><a href="#contact"></a>Contact</li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#portfolio">Experience</a></li>
+        <li><a href="#hobbies">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebookF/></a>
-        <a href="https://instagram.com"><FiInstagram/></a>
-        <a href="https://twitter.com"><IoLogoTwitter/></a>
+        <a href="https://www.facebook.com/felipe.emmanuel.ventura/"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/fventura16/"><FiInstagram/></a>
       </div>
 
       <div className="footer__copyright">
