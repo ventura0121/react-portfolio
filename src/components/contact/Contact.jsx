4ca75159abee062adb8cd6bc4496455d4fbc81f8
ventura0121 <ventura0121@gmail.com>
2022-09-19 21:01:30 -0400

    Made contact functionable

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import './Contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {BsLinkedin} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_z2xy3f4', 'template_lz65lga', form.current, '4fP5qn91TGQczPZw9')
+
+    e.target.reset()
+};
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -24,7 +34,7 @@ const Contact = () => {
             <a href="https://linkedin.com/in/felipeventura16" target="_blank">Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
