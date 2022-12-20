3ce408ffcc5970eee8307987bdbe12c1aa283472
ventura0121 <ventura0121@gmail.com>
2022-12-19 19:10:05 -0500

    Updated Resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/CV.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
